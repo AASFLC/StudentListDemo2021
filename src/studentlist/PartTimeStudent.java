1a9f3c36a62d9c7a17e41986ce31e8798b71cba3
Andra <Andra@10.0.0.195>
2021-05-23 20:30:40 -0400

    Initial commit

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,28 @@
+package studentlist;
+
+/**
+ *
+ * @author Andra
+ */
+public class PartTimeStudent extends Student
+{
+    private int numCourses;
+
+    public PartTimeStudent(int numCourses, String name)
+    {
+        super(name);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses()
+    {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses)
+    {
+        this.numCourses = numCourses;
+    }
+    
+    
+}
---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,41 @@
+package studentlist;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andraws
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);  
+        
+        //change the Array to 2
+        Student[] students = new Student[2];
+        
+        System.out.println("Enter student names");
+        
+        for (int i = 0 ; i < students.length ; i++)
+        {
+            System.out.println("Enter name for student " + (i + 1));
+            String name = input.nextLine();
+            Student student = new Student(name);
+            students[i] = student;
+        }
+        
+        
+        
+        String format = "Student name is %s\n";
+        
+        for (Student student: students)
+        {
+            System.out.printf(format, student.getName());
+        }
+        
+    }
+    
+}
